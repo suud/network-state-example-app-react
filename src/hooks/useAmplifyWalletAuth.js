81bdd0ed081a360e3b1ed014f95569ef4da2da9b
Timo Sutterer <hi@timo-sutterer.de>
2022-01-27 10:51:21 +0100

    feat: create random password

--- src/hooks/useAmplifyWalletAuth.js
+++ src/hooks/useAmplifyWalletAuth.js
@@ -1,6 +1,27 @@
 import { useState, useMemo, useEffect } from "react";
 import Amplify, { Auth } from "aws-amplify";
 
+function getStrongPassword(length, minUpper = 1, minLower = 1, minNumber = 1, minSpecial = 1) {
+    let upper = String.fromCharCode(...Array(91).keys()).slice(65), // A-Z
+        lower = String.fromCharCode(...Array(123).keys()).slice(97), // a-z
+        numbers = String.fromCharCode(...Array(58).keys()).slice(48), // 0-9
+        special = String.fromCharCode(...Array(127).keys()).slice(33).replace(/\w/g, ''), // special chars
+        all = upper + lower + numbers + special; // all characters
+    let minRequired = minSpecial + minUpper + minLower + minNumber;
+    let rs = [].concat(
+        Array.from({ length: minSpecial ? minSpecial : 0 }, () => special[Math.floor(Math.random() * special.length)]),
+        Array.from({ length: minUpper ? minUpper : 0 }, () => upper[Math.floor(Math.random() * upper.length)]),
+        Array.from({ length: minLower ? minLower : 0 }, () => lower[Math.floor(Math.random() * lower.length)]),
+        Array.from({ length: minNumber ? minNumber : 0 }, () => numbers[Math.floor(Math.random() * numbers.length)]),
+        Array.from({ length: Math.max(length, minRequired) - (minRequired ? minRequired : 0) }, () => all[Math.floor(Math.random() * all.length)]),
+    );
+    return rs.sort(() => Math.random() > 0.5).join('');
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 function useAmplifyWalletAuth(awsconfig) {
     const [user, setUser] = useState(undefined);
     const [token, setToken] = useState(undefined);
@@ -38,10 +59,11 @@ function useAmplifyWalletAuth(awsconfig) {
 
     // Sign up
     const signUp = (pubKey) => {
+        const passwordLength = getRandomInt(12, 32)
+        const password = getStrongPassword(passwordLength);
         const params = {
             username: pubKey,
-            // TODO: create random password
-            password: "123abcDEF?",
+            password: password,
         };
         return auth.signUp(params);
     }
